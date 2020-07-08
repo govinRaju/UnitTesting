1ee0bb746fab4f994b67cb948dd257194d72308c
Govind vuppu <vuppugovind3@gmail.com>
2020-07-07 20:11:42 -0500

    e2e using Puppeteer

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require('./util');
 
 //unit testing
@@ -18,3 +19,26 @@ test('should generate a valid text output', () => {
     const text = checkAndGenerate('Max', 29);
     expect(text).toBe('Max (29 years old)');
 });
+
+//Puppeteer
+//having some issue in the node modules have to check and fix it 
+
+test('should create an element with text and class', async () => {
+    const browser = puppeteer.launch({
+        headless: false,
+        // slowMo: 80,
+        // args: ['--window-size=1920,1080']
+    });
+    const page = await browser.nextPage();
+    await page.goto(
+        'C:/Users/ayyap/Documents/allmix/ReactApp/UnitTesting/index.html'
+    );
+    await page.click('input#name');
+    await page.type('input#name', 'Anna');
+    await page.click('input#name');
+    await page.type('input#age', '29');
+    await page.click('input#age');
+    await page.click('#btnAddUser');
+    const finalText = await page.$eval('.user-item', el => el.textContent);
+    expect(finalText).toBe('Anna (28 years old)');
+}, 10000);
